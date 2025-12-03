7a5ef53ffeb03eda168e91e7cf3af66e87deb683
yinggangtian <ytia0059@student.monash.edu>
2025-12-03 17:55:21 +1100

    add test case

---
+++ scripts/guihua_test.sh
@@ -0,0 +1,3 @@
+uv run python scripts/test_api.py "78，21，15点，女生，我现在正在一家广告公司，请问能不能和领导提升职？" >1.md
+uv run python scripts/test_api.py "35，72，下午3点，男生，我现在正在一家广告公司，请问能不能和领导提升职？" >./2.md
+uv run python scripts/test_api.py "，男生，56，94846，我现在正在一家广告公司，请问能不能在年底拿到奖金" >./3.md
